f61b7dc7dc0b72020e4c2cb8588a3c94a99990ac
ahmed <rifkiahmed.91@gmail.com>
2023-06-06 06:52:23 -0700

    Python script that prints the last digit of the number stored in a number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+modulo = number % 10
+if modulo == 0:
+    print(f"Last digit of {number} is {modulo} and is 0")
+elif modulo < 0:
+    print(f"Last digit of {number} is {modulo} and is less than 6 and not 0")
+else:
+    print(f"Last digit of {number} is {modulo} and is greater than 5")
