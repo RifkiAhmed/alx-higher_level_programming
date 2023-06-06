c28b7ecf826adbbb66d522e4619636d23a5df74f
ahmed <rifkiahmed.91@gmail.com>
2023-06-06 09:13:25 -0700

    Correct code for negatives numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-modulo = number % 10
+if number < 0:
+    modulo = (-number)%10 * -1
+else:
+    modulo = number % 10
 if modulo == 0:
     print(f"Last digit of {number} is {modulo} and is 0")
 elif modulo < 0:
