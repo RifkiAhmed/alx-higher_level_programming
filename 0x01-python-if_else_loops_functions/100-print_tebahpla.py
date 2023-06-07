358c40754cf5099696a9aa4b7fdf1b0469a23937
ahmed <rifkiahmed.91@gmail.com>
2023-06-07 13:32:05 -0700

    Use print() function with string format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 i = 122
 while i >= 97:
-    print(chr(i), end="") if i % 2 == 0 else print(chr(65 + i - 97), end="")
+    print("{0}".format(chr(i)), end="") if i % 2 == 0 else print("{0}".format(chr(65 + i - 97)), end="")
     i-= 1
