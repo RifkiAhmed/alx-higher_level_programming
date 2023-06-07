c65a7e43dc60493e7c5ba56ba8ef01cae81771a9
ahmed <rifkiahmed.91@gmail.com>
2023-06-07 13:36:55 -0700

    Add indentation

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,5 +2,5 @@
 i = 122
 while i >= 97:
     print("{0}".format(chr(i)), end="") if i % 2 == 0 else\
-    print("{0}".format(chr(65 + i - 97)), end="")
+        print("{0}".format(chr(65 + i - 97)), end="")
     i -= 1
