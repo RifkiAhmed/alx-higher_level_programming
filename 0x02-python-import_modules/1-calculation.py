120aaadfee29e6be25b1d54642e6ed6c52f9d711
ahmed <rifkiahmed.91@gmail.com>
2023-06-08 13:13:45 -0700

    Delete whitespace before comma

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ if __name__ != "__main__":
     exit(0)
 a = 10
 b = 5
-print("{0:d} + {1:d} = {2:d}".format(a, b , add(a, b)))
-print("{0:d} - {1:d} = {2:d}".format(a, b , sub(a, b)))
-print("{0:d} * {1:d} = {2:d}".format(a, b , mul(a, b)))
-print("{0:d} / {1:d} = {2:d}".format(a, b , div(a, b)))
+print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
+print("{0:d} - {1:d} = {2:d}".format(a, b, sub(a, b)))
+print("{0:d} * {1:d} = {2:d}".format(a, b, mul(a, b)))
+print("{0:d} / {1:d} = {2:d}".format(a, b, div(a, b)))
