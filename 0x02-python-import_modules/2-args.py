1bac136fe0c4f54581bb6dce08fe8fc5c986d94f
ahmed <rifkiahmed.91@gmail.com>
2023-06-08 14:29:58 -0700

    Add whitespace around operator

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ != "__main__":
     exit(0)
 argc = len(argv)
 print("{0:d}".format(argc - 1), end="")
-print(" argument" if argc <3 else " arguments", end="")
-print("." if argc <2 else ":")
+print(" argument" if argc == 2 else " arguments", end="")
+print("." if argc < 2 else ":")
 for i in range(1, argc):
     print("{0}: {1}".format(i, argv[i]))
