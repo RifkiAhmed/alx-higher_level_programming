02db7dd53e69abbf7053e6192d03d99ad5528a29
ahmed <rifkiahmed.91@gmail.com>
2023-06-10 09:43:43 -0700

    Change none to None

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0 or idx == len(my_list):
-        print("none")
+        print None
     else:
         print("Element at index {0} is {1}".format(idx, my_list[idx]))
