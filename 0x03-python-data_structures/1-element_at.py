779b6b1753b6b0744d31ab96ca6624a5c0633a7c
ahmed <rifkiahmed.91@gmail.com>
2023-06-10 09:49:02 -0700

    change print() by retrun

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,4 +3,4 @@ def element_at(my_list, idx):
     if idx < 0 or idx == len(my_list):
         return None
     else:
-        print("Element at index {0} is {1}".format(idx, my_list[idx]))
+        return my_list[idx])
