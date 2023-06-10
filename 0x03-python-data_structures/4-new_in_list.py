711534bda4305bb511e189da71f73b02bfe2949d
ahmed <rifkiahmed.91@gmail.com>
2023-06-10 12:08:42 -0700

    Add retrun of new_list after modifying

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,6 @@
 def new_in_list(my_list, idx, element):
     if my_list:
         new_list = my_list[:]
-        if idx < 0 or idx > len(my_list):
-            return new_list
-        else:
+        if idx in [0, len(my_list)]:
             new_list[idx] = element
+        return new_list
