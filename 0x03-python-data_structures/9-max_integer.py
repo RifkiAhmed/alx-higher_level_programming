ee3c05fededc7ea20879c3478c724cdd60bd49a4
ahmed <rifkiahmed.91@gmail.com>
2023-06-10 19:08:49 -0700

    Add check if all element are less than zero

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        max = 0
+        max = my_list[0]
         for i in my_list:
             if max < i:
                 max = i
