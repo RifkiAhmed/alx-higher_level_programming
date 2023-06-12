b53ea85599f0bcbf755fed2d46760e7957a7bc60
ahmed <rifkiahmed.91@gmail.com>
2023-06-12 16:25:08 -0700

    Use a buffer of size 1024

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,36 +9,27 @@
 int is_palindrome(listint_t **head)
 {
 	int len = 0, i;
-	int *data;
+	int *data[BUF];
 
 	if (*head == NULL)
 		return (1);
 	if ((*head)->next == NULL)
 		return (1);
-	data = malloc(sizeof(int));
 	while ((*head)->next != NULL)
 	{
 		data[len] = (*head)->n;
 		len++;
-		data = realloc(data, sizeof(int) * len);
 		*head = (*head)->next;
 	}
 	data[len] = (*head)->n;
 
 	if (len % 2 == 0)
-	{
-		free(data);
 		return (0);
-	}
 
 	for (i = 0; i <= (len / 2); i++)
 	{
 		if (data[i] != data[len - i])
-		{
-			free(data);
 			return (0);
-		}
 	}
-	free(data);
 	return (1);
 }
