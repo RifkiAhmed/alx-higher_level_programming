daaa1a69b7b7f1b84060acffb789855b0b6e1644
Ahmed <rifkiahmed.91@gmail.com>
2023-06-12 22:36:16 +0000

    Fix betty style checks

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -5,13 +5,13 @@
 void print_python_list_info(PyObject *p)
 {
 	int len = 0, i;
+
 	len = (int)PyList_Size(p);
 	printf("[*] Size of the Python List = %d\n", len);
 	printf("[*] Allocated = \n");
 
 	for (i = 0 ; i < len; i++)
 	{
-		printf("Element %d: %s\n",i, (char *)*(Py_TYPE(p))->PyType_Type);
-		
+		printf("Element %d: %s\n", i, (char *)*(Py_TYPE(p))->PyType_Type);
 	}
 }
