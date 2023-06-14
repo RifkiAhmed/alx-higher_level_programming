558750036657392cf9e8f0e9838f54f554f0915b
ahmed <rifkiahmed.91@gmail.com>
2023-06-14 15:22:13 -0700

    Make a copy of matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix == [[]]:
-        return [[]]
-    for row in matrix:
+        return None
+    matrix2 = []
+    for i in range(len(matrix)):
+        matrix2 = matrix2 + [matrix[i][:]]
+    for row in matrix2:
         for i in range(len(row)):
             row[i] = row[i] ** 2
-    return matrix
+    return matrix2
