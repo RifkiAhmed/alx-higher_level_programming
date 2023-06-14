79bc7cf27da6b6bf6b0cc4589ea4df4ee70e71b9
ahmed <rifkiahmed.91@gmail.com>
2023-06-14 14:30:10 -0700

    Handle emty matrix checks

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if matrix == [[]]:
+        return [[]]
     for row in matrix:
         for i in range(len(row)):
             row[i] = row[i] ** 2
