a5d57274509f94f49916c4f1686e77e3ae6ec4ed
ahmed <rifkiahmed.91@gmail.com>
2023-06-14 17:40:15 -0700

    Set of all elements present in only one set

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    my_list = list(set_1) + list(set_2)
+    my_list2 = []
+    for elem in my_list:
+        if elem in set_2 and  elem in set_1:
+            continue
+        my_list2.append(elem)
+    return set(my_list2)
