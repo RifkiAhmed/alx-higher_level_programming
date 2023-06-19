39cdb97a33c17fd846e6eb62f15070988b126b52
ahmed <rifkiahmed.91@gmail.com>
2023-06-19 00:07:25 +0000

    Correct variable name

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return None
     first_key = list(a_dictionary)[0]
-    key_name = fir
+    key_name = first_key[:]
     for key in a_dictionary:
         if a_dictionary[first_key] < a_dictionary[key]:
             a_dictionary[first_key] = a_dictionary[key]
