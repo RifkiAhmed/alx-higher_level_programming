f76d458366956c6ed360a8e2a9a89ad20a85990c
ahmed <rifkiahmed.91@gmail.com>
2023-06-26 22:25:34 +0000

    Use Exception base class instead of ValueError exception type

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err:
+    except Exception as err:
         sys.stderr.write("Exception: {0}\n".format(err.args[0]))
         return False
