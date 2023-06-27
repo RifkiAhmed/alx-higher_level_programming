11b4bf358daf1d65889a02f78aff828981f5f810
ahmed <rifkiahmed.91@gmail.com>
2023-06-27 22:03:03 +0000

    Add private instance attribute position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+'''
+class Square: new type
+'''
+
+
+class Square:
+    ''' Represents a square with a zise and position.'''
+
+    def __init__(self, size=0, position=(0, 0)):
+        '''Initialise instance/object'''
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        ''' Return the current square area'''
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        ''' Return current square size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        ''' Set current square size'''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        ''' Returns current square position'''
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        ''' Sets current square position'''
+        if (not isinstance(value[0], int) or not isinstance(value[1], int)) \
+                and (value[0] <= 0 or value[1] <= 0):
+            raise TypeError("position must be a tuple of 2 positive \
+                            integers")
+
+    def my_print(self):
+        ''' Prints in stdout the square
+            if size is diffrent from 0 prints with the character #
+            otherwise prints an empty line
+        '''
+        if self.__size == 0:
+            print('')
+        else:
+            space = ''
+            if self.__position[1] == 0:
+                i = 0
+                while (self.__position[0] >= i):
+                    space += ' '
+                    i += 1
+            for i in range(self.__size):
+                print("{:s}".format(space), end="")
+                for j in range(self.__size):
+                    print('#', end="")
+                print('')
