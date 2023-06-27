1af1987e7e8f3ffd944b488e69de12c2b46bc6fe
ahmed <rifkiahmed.91@gmail.com>
2023-06-27 22:11:18 +0000

    Deletes checks from __init__ method

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -10,12 +10,7 @@ class Square:
 
     def __init__(self, size=0):
         '''Initialise instance/object'''
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
         ''' Return the current square area'''
