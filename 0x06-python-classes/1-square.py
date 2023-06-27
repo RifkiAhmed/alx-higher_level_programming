ea9279b35ca0080ea38faa34cabc8c982c6c6b55
ahmed <rifkiahmed.91@gmail.com>
2023-06-27 06:21:24 -0700

    Creates a class Square that instantiates a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''
+class Square: new type
+'''
+
+
+class Square:
+    ''' Represents a square with a zise.'''
+
+    def __init__(self, size):
+        '''Initialise instance/object'''
+        self.__size = size
