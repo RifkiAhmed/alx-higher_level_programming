39bb3654c625b246e4aa4accb4a2f73409c6e42d
ahmed <rifkiahmed.91@gmail.com>
2023-06-27 23:56:48 +0000

    Fix checks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 
-'''
-class Square: new type
+''' Class Square: Definition of new type
 '''
 
 
 class Square:
-    ''' Represents a square with a zise.'''
+    ''' Represents a square with a zise.
+    '''
 
     def __init__(self, size=0, position=(0, 0)):
-        '''Initialise instance/object'''
+        ''' Initialise instance/object
+        '''
         self.__size = size
         self.__position = position
 
     def area(self):
-        ''' Return the current square area'''
+        ''' Return the current square area
+        '''
         return self.__size ** 2
 
     def my_print(self):
-        ''' Prints square in stdout'''
+        ''' Prints square in stdout
+        '''
         if self.__size == 0:
             print()
         else:
@@ -28,12 +31,14 @@ class Square:
 
     @property
     def size(self):
-        ''' Return current square size'''
+        ''' Return current square size
+        '''
         return self.__size
 
     @size.setter
     def size(self, value):
-        ''' Set current square size'''
+        ''' Set current square size
+        '''
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -43,11 +48,14 @@ class Square:
 
     @property
     def position(self):
-        ''' Returns current square position'''
+        ''' Returns current square position
+        '''
         return self.__position
 
     @position.setter
     def position(self, value):
+        ''' Sets current square position
+        '''
         if not (isinstance(value, tuple) and
                 len(value) == 2 and
                 isinstance(value[0], int) and
