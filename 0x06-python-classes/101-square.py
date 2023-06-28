e240dd9a56cf7d8855c4a8cb316bb74dc20fd043
ahmed <rifkiahmed.91@gmail.com>
2023-06-28 22:16:09 +0000

    Fix checks

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -33,10 +33,10 @@ class Square:
         ''' Prints square format
         '''
         if self.size:
-            str = ''
-            str += '\n' * (self.position[1])
-            for i in range(self.size):
+            str = '\n' * (self.position[1])
+            for i in range(self.size - 1):
                 str += ' ' * self.position[0] + '#' * self.size + '\n'
+            str += ' ' * self.position[0] + '#' * self.size
             return str
         else:
             return str()
