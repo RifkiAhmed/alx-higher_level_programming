752845cb71353086a884f08bf1f5baba426235f0
ahmed <rifkiahmed.91@gmail.com>
2023-06-29 02:27:50 +0000

    add strdoc for instance methods

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -9,14 +9,21 @@ import math
 class MagicClass:
     ''' Definition of class MagicClass
     '''
+
     def __init__(self, radius=0):
+        ''' Initialises current instance
+        '''
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
+        ''' Returns area of current instance
+        '''
         return (self.__radius ** 2) * math.pi
 
     def circumference(self):
+        ''' Returns the circumference of current instance
+        '''
         return 2 * math.pi * self.__radius
