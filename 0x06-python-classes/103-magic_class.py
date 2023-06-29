bc146907f703d66fee3f98755edd586a19e580a7
ahmed <rifkiahmed.91@gmail.com>
2023-06-29 02:11:28 +0000

    Class that do the same as a given python bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -8,7 +8,6 @@ class MagicClass:
     ''' Definition of class MagicClass
     '''
     def __init__(self, radius):
-        self.__radius = 0
         if type(radius) is not int or type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
