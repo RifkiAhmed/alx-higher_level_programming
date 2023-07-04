28700d3166f8e797463704d728b4d761ce8adb82
ahmed <rifkiahmed.91@gmail.com>
2023-07-04 05:20:38 -0700

    Add docstrings to instance methods

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,15 +8,21 @@ class Rectangle:
     '''
 
     def __init__(self, width=0, height=0):
+        ''' Initialize instance
+        '''
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        ''' Gets width value
+        '''
         return with
 
     @width.setter
     def width(self, value):
+        ''' Sets width
+        '''
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -25,12 +31,16 @@ class Rectangle:
 
     @property
     def height(self):
+        ''' Gets height value
+        '''
         return height
 
     @height.setter
     def height(self, value):
+        ''' Sets height
+        '''
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
+        self.__height = value
