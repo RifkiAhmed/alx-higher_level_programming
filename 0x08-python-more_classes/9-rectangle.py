277ae91cb31196238fde2ae6cea3e1ee493671d8
ahmed <rifkiahmed.91@gmail.com>
2023-07-05 18:34:43 -0700

    Uses Rectangle class declaration

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -95,4 +95,4 @@ class Rectangle:
     def square(cls, size=0):
         ''' Returns a new Rectangle instance with width == height == size
         '''
-        return cls(width=size, height=size)
+        return Rectangle(width=size, height=size)
