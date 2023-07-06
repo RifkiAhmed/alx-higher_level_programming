4703c1db4117c1977558f12a0f51a43a068b9eee
ahmed <rifkiahmed.91@gmail.com>
2023-07-05 18:18:19 -0700

    Corrects TypeError syntax

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -85,7 +85,7 @@ class Rectangle:
         if type(rect_1) is not Rectangle:
             raise TypeError('rect_1 must be an instance of Rectangle')
         if type(rect_2) is not Rectangle:
-            raise Typeerror('rect_2 must be an instance of Rectangle')
+            raise TypeError('rect_2 must be an instance of Rectangle')
         if rect_1.area() >= rect_2.area():
             return rect_1
         else:
