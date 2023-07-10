fd73930d805da1e22fc0d8e1e210e5b4877090b8
ahmed <rifkiahmed.91@gmail.com>
2023-07-10 06:37:53 +0000

    Add strdoc for module

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+''' Module for a function that returns list of attributes
+    and methods of an object
+'''
+
+
 def lookup(obj):
     ''' Gets a list of attributes and methods of an object
     '''
