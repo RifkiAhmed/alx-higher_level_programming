f7c60debb42b20ecd94a8e75d68df3924c4e4f79
ahmed <rifkiahmed.91@gmail.com>
2023-07-10 17:17:27 +0000

    Add instance method integer_validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' Module for an class BaseGeometry
+'''
+
+
+class BaseGeometry(object):
+    ''' Represents a BaseGeometry class
+    '''
+
+    def area(self):
+        '''  Gets area of current instance or raise Exception
+        '''
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        ''' Validates value; value must be an integer and greater than 0
+        '''
+        self.name = name
+        if type(value) is not int:
+            raise TypeError(str(name) + ' must be an integer')
+        elif value <= 0:
+            raise ValueError(str(name) + ' must be greater than 0')
+        else:
+            self.value = value
