0c3f18cbe1cebfd7e3c158e4a3df9e41501c2597
ahmed <rifkiahmed.91@gmail.com>
2023-07-10 20:26:12 +0000

    Add add_attribute() function

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' Module defines a function add_attribute()
+    adds attribute to an object
+    else raise Typeerror with message: can't add new attribute
+'''
+
+
+def add_attribute(obj, name, value):
+    ''' Adds an attribute to obj if possible
+    '''
+    if "__dict__" in dir(obj):
+        onj.__setattr__(name, value)
+    else:
+        raise TypeError("can't add new attribute")
