e4b454d4b1309edf8e02cba14bd01ab96b1c07e0
ahmed <rifkiahmed.91@gmail.com>
2023-07-10 08:00:31 +0000

    Add public method area()

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' Module for an class BaseGeometry
+'''
+
+
+class BaseGeometry(object):
+    ''' Represents a BaseGeometry class
+    '''
+
+    def area(self):
+        '''  Gets area of current instance or raise Exception
+        '''
+        raise Exception('area() is not implemented')
