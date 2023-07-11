23b03341f04a0291448a3366622ca6065c01c2a2
ahmed <rifkiahmed.91@gmail.com>
2023-07-11 21:15:36 +0000

    Deletes json module

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 ''' Module for writing dictionary representation of a class object
 '''
-import json
 
 
 def class_to_json(obj):
     ''' Writes dictionary representation of a class object
     '''
-    s = json.dumps(obj.__dict__)
-    return json.loads(s)
+    return obj.__dict__
