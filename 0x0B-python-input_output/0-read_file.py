a1999166860c6da80c40c3df69627bdf13aa436c
ahmed <rifkiahmed.91@gmail.com>
2023-07-11 07:11:59 -0700

    Reads text file UTF8

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' Module for reading a text file UTF-8
+'''
+
+
+def read_file(filename=""):
+    ''' Reads a text file UTF-8
+    '''
+    with open(filename, encoding="utf-8") as text:
+        print(text.read())
