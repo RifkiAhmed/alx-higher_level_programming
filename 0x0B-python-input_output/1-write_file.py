42c99594ff171db67d9b0c2017a3fabd8e0d9141
ahmed <rifkiahmed.91@gmail.com>
2023-07-11 07:26:24 -0700

    writes to a text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' Module for reading a text file UTF-8
+'''
+
+
+def write_file(filename="", text=""):
+    ''' Reads a text file UTF-8
+    '''
+    with open(filename, encoding="utf-8", mode="w") as file1:
+        return file1.write(text)
