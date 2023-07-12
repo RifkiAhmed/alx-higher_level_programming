5b8c993c79acc3b6e4ba9b447be8889e41396a1e
ahmed <rifkiahmed.91@gmail.com>
2023-07-12 01:53:36 +0000

    Fix checks

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,8 +7,9 @@ def pascal_triangle(n):
     ''' Prints Pascal triangle
     '''
     if n <= 0:
-        return [[]]
-    m = []
-    for i in range(n):
-        m.append(list(int(c) for c in str(11 ** i)))
-    return m
+        return []
+    else:
+        m = []
+        for i in range(n):
+            m.append(list(int(c) for c in str(11 ** i)))
+        return m
