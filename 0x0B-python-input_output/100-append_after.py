b2b4309a823f8ac5c9646422544d5c9607789fd2
ahmed <rifkiahmed.91@gmail.com>
2023-07-12 00:40:26 +0000

    use utf-8

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -9,10 +9,10 @@ def append_after(filename="", search_string="", new_string=""):
         line containing a specific string
     '''
     text = ""
-    with open(filename, encoding="utf8", mode="r") as file13:
+    with open(filename, encoding="utf-8", mode="r") as file13:
         for line in file13:
             text += line
             if search_string in line:
                 text += new_string
-    with open(filename, encoding="utf8", mode="w") as file13:
+    with open(filename, encoding="utf-8", mode="w") as file13:
         file13.write(text)
