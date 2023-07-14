cb4fcb48cae6bbedc63c687f64f827b6b9542448
ahmed <rifkiahmed.91@gmail.com>
2023-07-14 15:55:42 -0700

    Add Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' Module for Square class that inherits from rectangle class
+'''
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    ''' Representing Square class'''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        ''' Initialize instance '''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        ''' Returns a string representation of Square instance '''
+        return "[Square] ({0}) {1}/{2} - {3}".format(
+            self.id,
+            self.x,
+            self.y,
+            self.width)
