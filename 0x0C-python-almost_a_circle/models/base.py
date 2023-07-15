aa6a9e921b1ae70c1703abb5efd4e1269f16b941
ahmed <rifkiahmed.91@gmail.com>
2023-07-15 06:37:09 -0700

    Add create() class method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,33 +6,48 @@ import json
 
 class Base:
     ''' Representing Base class '''
-    __nb_objects = 0
+    __nb_instanceects = 0
 
     def __init__(self, id=None):
         ''' Initialize instance '''
         if id is not None:
             self.id = id
         else:
-            type(self).__nb_objects += 1
-            self.id = type(self).__nb_objects
+            type(self).__nb_instanceects += 1
+            self.id = type(self).__nb_instanceects
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ Returns the JSON string representation of list_dictionaries """
+        ''' Returns the JSON string representation of list_dictionaries '''
         if list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
-    def save_to_file(cls, list_objs):
-        """ Writes the JSON string representation of list_objs to a file """
+    def save_to_file(cls, list_instances):
+        ''' Writes the JSON string representation of list_instances to a file
+        '''
         with open("Rectangle.json", encoding="utf-8", mode="w") as file:
             file.write(cls.to_json_string(list(
-                cls.to_dictionary(ob) for ob in list_objs)))
+                cls.to_dictionary(ob) for ob in list_instances)))
 
     @staticmethod
     def from_json_string(json_string):
-        """ Returns the list of the JSON string representation json_string """
+        ''' Returns the list of the JSON string representation json_string '''
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''Loads instance from dictionary.'''
+        from rectangle import Rectangle
+        from square import Square
+
+        instance = None
+        if cls is Rectangle:
+            instance = Rectangle(1, 1)
+        elif cls is Square:
+            instance = Square(1)
+        instance.update(**dictionary)
+        return instance
