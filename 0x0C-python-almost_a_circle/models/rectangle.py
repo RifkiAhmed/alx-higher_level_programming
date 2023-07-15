d6a1885822a5709d07b3d2b2ac165dc051e14eca
ahmed <rifkiahmed.91@gmail.com>
2023-07-14 17:45:57 -0700

    Change update() instance  method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,6 @@
 ''' Module for Rectangle class that inherits from Base class
 '''
 from base import Base
-import json
 
 
 class Rectangle(Base):
@@ -27,19 +26,13 @@ class Rectangle(Base):
 
     def update(self, *args, **kwargs):
         ''' Updates instances attributtes '''
-        if len(args) >= 1:
-            self.id = args[0]
-            if len(args) >= 2:
-                self.__width = args[1]
-            if len(args) >= 3:
-                self.__height = args[2]
-            if len(args) >= 4:
-                self.__x = args[3]
-            if len(args) >= 5:
-                self.__y = args[4]
+        tp = ("id", "width", "height", "x", "y")
+        if args:
+            for i, item in enumerate(args):
+                self.__setattr__(tp[i], args[i])
         else:
             for key in kwargs:
-                if key in ['id', 'width', 'height', 'x', 'y']:
+                if key in tp:
                     self.__setattr__(key, kwargs[key])
 
     def __str__(self):
