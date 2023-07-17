a14267c5d711a8bb430f7111f1c8e815d0cd9923
ahmed <rifkiahmed.91@gmail.com>
2023-07-17 18:35:55 +0000

    Changes to_json_string()

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         ''' Returns JSON string representation of list_dictionaries '''
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == []:
             return '[]'
         return dumps(list_dictionaries)
 
