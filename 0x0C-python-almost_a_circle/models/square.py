16718445ec1d4aae14b3212fc91f31dbeb13c9e7
ahmed <rifkiahmed.91@gmail.com>
2023-07-17 17:33:23 +0000

    Fix model package import

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ''' Module for Square class that inherits from rectangle class
 '''
-from rectangle import Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
