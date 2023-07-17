80f823eea04c6435fb04ea0f7321903aeceda3ba
ahmed <rifkiahmed.91@gmail.com>
2023-07-17 23:17:00 +0000

    Add models package

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 ''' Module for Base class '''
+from models.rectangle import Rectangle
+from models.square import Square
 from json import dumps, loads
 from csv import writer, reader
 
