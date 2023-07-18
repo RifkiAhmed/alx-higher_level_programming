6b4ab8e5be316ba4f362c730c49390bbfe16ccd1
ahmed <rifkiahmed.91@gmail.com>
2023-07-18 18:28:23 +0000

    Add tests cases for Base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -10,102 +10,62 @@ from models.square import Square
 class BaseTest(unittest.TestCase):
     ''' class for testing Base class '''
 
-    def setUp(self):
-        ''' Instanciates Base class '''
-        self.base1 = Base(99)
+    def test__init__NoneId(self):
+        ''' Test __init__() instance method'''
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
 
-    def tearDown(self):
-        ''' Removes Base class instance '''
-        del self.base1
+    def test__init__IntId(self):
+        ''' Test __init__() instance method'''
+        b2 = Base(99)
+        self.assertEqual(b2.id, 99)
 
-    def test_id(self):
-        ''' Test instance attribute "id" '''
-        base2 = Base()
-        self.assertEqual(base2.id, 11)
-        self.assertEqual(self.base1.id, 99)
-        self.assertIs(self.base1.id, 99)
+    def test__init__TypeError(self):
+        ''' Test __init__() instance method'''
+        self.assertRaises(TypeError, Base, *(1, 2))
 
-    def test_init1(self):
-        ''' Test __init__() with 2 args'''
-        args = (1, 2)
-        self.assertRaises(TypeError, Base, *args)
-
-    def test_init2(self):
-        ''' Test __init__() with one and two key words arguments '''
+    def test__init_ListId(self):
+        ''' Test __init__() instance method'''
         kwargs = {'id': [1, 2]}
         self.assertEqual(Base(**kwargs).id, [1, 2])
-        self.assertIsInstance(Base(**kwargs).id, list)
-        self.assertNotIsInstance(Base(**kwargs).id, int)
-        kwargs['dummy_arg'] = 1
-        self.assertRaises(TypeError, Base, **kwargs)
 
-    def test_instance(self):
-        ''' Test Base instance type '''
-        self.assertIsInstance(self.base1, Base)
+    def test__init__TypeError(self):
+        ''' Test __init__() instance method'''
+        self.assertRaises(TypeError, Base, *(1, 2))
 
-    def test_eq(self):
-        ''' Test two Base instances non equality '''
-        base1 = Base()
-        base2 = Base()
-        self.assertFalse(base1 == base2)
-
-    def test__Rectangle_to_json_string(self):
+    def test__toJsonString(self):
         ''' Test to_json_string() static method '''
-        r1 = Rectangle(10, 7, 2, 48)
-        r2 = Rectangle(10, 7)
-        d1 = r1.to_dictionary()
-        self.assertEqual(dumps([d1]), Base.to_json_string([d1]))
-        d2 = r2.to_dictionary()
-        list_dictionaries = [d1, d2]
+        list_dictionaries = [{"id": 7}, {"id": 10}]
         self.assertEqual(
-                dumps([d1, d2]), Base.to_json_string(list_dictionaries))
-        self.assertNotIsInstance(type(r1), type(Base.to_json_string([d1])))
+                dumps(list_dictionaries),
+                Base.to_json_string(list_dictionaries))
+
+    def test__toJsonStringEmptyList(self):
+        ''' Test to_json_string() static method '''
         self.assertEqual('[]', Base.to_json_string([]))
+
+    def test__toJsonStringNoneList(self):
+        ''' Test to_json_string() static method '''
         self.assertEqual('[]', Base.to_json_string(None))
+
+    def test__toJsonStringTypeError(self):
+        ''' Test to_json_string() static method '''
         self.assertRaises(TypeError, Base.to_json_string)
 
-    def test__Square_to_json_string(self):
-        ''' Test to_json_string() static method '''
-        s1 = Square(10, 7, 2)
-        s2 = Square(10)
-        d1 = s1.to_dictionary()
-        self.assertEqual(dumps([d1]), Base.to_json_string([d1]))
-        self.assertIsInstance(Base.to_json_string([d1]), str)
-        d2 = s2.to_dictionary()
-        list_dictionaries = [d1, d2]
-        self.assertEqual(
-                dumps([d1, d2]), Base.to_json_string(list_dictionaries))
-        self.assertNotIsInstance(type(s1), type(Base.to_json_string([d1])))
-        self.assertEqual('[]', Base.to_json_string([]))
-        self.assertEqual('[]', Base.to_json_string(None))
-        self.assertRaises(TypeError, Base.to_json_string)
-
-    def test__Rectangle_from_json_string(self):
+    def test__fromJsonString(self):
         ''' Test from_json_string() static method '''
-        r1 = Rectangle(10, 7, 2, 48)
-        r2 = Rectangle(10, 7)
-        str1 = Base.to_json_string(r1.to_dictionary())
-        self.assertEqual(loads(str1), Base.from_json_string(str1))
-        list_dictionaries = [r1.to_dictionary(), r2.to_dictionary()]
-        str2 = Base.to_json_string(list_dictionaries)
-        self.assertEqual(
-                loads(str2), Base.from_json_string(str2))
-        self.assertNotIsInstance(type(r1), type(Base.from_json_string(str1)))
-        self.assertEqual([], Base.from_json_string(''))
-        self.assertEqual([], Base.from_json_string(None))
-        self.assertRaises(TypeError, Base.from_json_string)
+        list_dictionaries = [{"id": 7}, {"id": 10}]
+        str1 = Base.to_json_string(list_dictionaries)
+        self.assertEqual(list_dictionaries, Base.from_json_string(str1))
 
-    def test__Square_from_json_string(self):
+    def test__fromJsonEmptyString(self):
         ''' Test from_json_string() static method '''
-        s1 = Square(10, 7, 2)
-        s2 = Square(10)
-        str1 = Base.to_json_string(s1.to_dictionary())
-        self.assertEqual(loads(str1), Base.from_json_string(str1))
-        list_squares = [s1.to_dictionary(), s2.to_dictionary()]
-        str2 = Base.to_json_string(list_squares)
-        self.assertEqual(
-                loads(str2), Base.from_json_string(str2))
-        self.assertNotIsInstance(type(s1), type(Base.from_json_string(str1)))
         self.assertEqual([], Base.from_json_string(''))
+
+    def test__fromJsonNoneString(self):
+        ''' Test from_json_string() static method '''
         self.assertEqual([], Base.from_json_string(None))
+
+    def test__fromJsonStringTypeError(self):
+        ''' Test from_json_string() static method '''
         self.assertRaises(TypeError, Base.from_json_string)
