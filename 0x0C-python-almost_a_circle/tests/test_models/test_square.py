ba9b97421ddb4b47240f0e16ff1d0ed23dab9a1e
ahmed <rifkiahmed.91@gmail.com>
2023-07-18 20:57:18 +0000

    Add tests cases for square model

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -20,51 +20,49 @@ class TestSquare(unittest.TestCase):
         ''' Removes variable '''
         del self.args, self.kwargs
 
-    def test__init(self):
+    def test__Square(self):
         ''' Test __init__() instance method '''
-        s3 = Square(*self.args[: 1])
-        s4 = Square(*self.args[: 2])
-        s5 = Square(*self.args[: 3])
-        self.assertIsInstance(self.s1, Square)
-        self.assertEqual(self.s2.id, 99)
-        self.assertTrue(self.s1.width == self.s1.height)
-        self.assertGreater(s5.width, 0)
+        s3 = Square(1)
+        self.assertEqual(s3.width, 1)
+        self.assertEqual(s3.height, 1)
+        self.assertEqual(s3.x, 0)
+        self.assertEqual(s3.y, 0)
 
-    def test__typeError(self):
+    def test__SquareTypeError(self):
         ''' Test instance type errors '''
         self.assertRaises(TypeError, Square, *self.args[0: 0])
         self.assertRaises(TypeError, Square, *self.args)
 
-    def test__width_ValueError(self):
+    def test__Size_ValueError(self):
         ''' Test width value errors '''
         self.assertRaises(ValueError, Square, -10, 20, 30, 40)
         self.assertRaises(ValueError, Square, 0, 20, 30, 40)
 
-    def test__width_TypeError(self):
+    def test__Size_TypeError(self):
         ''' Test width type errors '''
         self.assertRaises(TypeError, Square, 'w', 20, 30, 40)
 
-    def test__x_ValueError(self):
+    def test__Square_x_ValueError(self):
         ''' Test x value errors '''
         self.assertRaises(ValueError, Square, 10, -20, 30, 40)
 
-    def test__x_TypeError(self):
+    def test__Square_x_TypeError(self):
         ''' Test x type errors '''
         self.assertRaises(TypeError, Square, 10, 'x', 30, 40)
 
-    def test__y_ValueError(self):
+    def test__Square_y_ValueError(self):
         ''' Test y value errors '''
         self.assertRaises(ValueError, Square, 10, 20, -30, 40)
 
-    def test__y_TypeError(self):
+    def test__Square_y_TypeError(self):
         ''' Test y type errors '''
         self.assertRaises(TypeError, Square, 10, 20, 'y', 40)
 
-    def test__area(self):
+    def test__SquareArea(self):
         ''' Test area() instance method '''
         self.assertEqual(self.s1.area(), 100)
 
-    def test__update(self):
+    def test__SquareUpdate(self):
         ''' Test update() instance method '''
         s6 = Square(*self.args[: 4])
         s7 = Square(**self.kwargs)
@@ -73,18 +71,18 @@ class TestSquare(unittest.TestCase):
         self.s2.update(*self.args[: 4])
         self.assertFalse(s7.id == self.s2.id)
 
-    def test__to_Dictionary(self):
+    def test__square_to_Dictionary(self):
         ''' Test to_dictionary() instance method '''
         self.s1.update(**self.kwargs)
         d1 = self.s1.to_dictionary()
         self.assertEqual(d1, self.kwargs)
 
-    def test__str(self):
+    def test__Square__str(self):
         ''' Test __str__() instance method '''
         output = '[Square] (40) 20/30 - 10'
         self.assertTrue(output == str(self.s1))
 
-    def test__toJsonString(self):
+    def test__squareToJsonString(self):
         ''' Test to_json_string() static method '''
         s1 = Square(10, 7, 2, 48)
         list_dictionaries = s1.to_dictionary()
@@ -92,33 +90,33 @@ class TestSquare(unittest.TestCase):
                 dumps(list_dictionaries),
                 Square.to_json_string(list_dictionaries))
 
-    def test__toJsonStringEmptyList(self):
+    def test__squareToJsonStringEmptyList(self):
         ''' Test to_json_string() static method '''
         self.assertEqual('[]', Square.to_json_string([]))
 
-    def test__toJsonStringNoneList(self):
+    def test__squareToJsonStringNoneList(self):
         ''' Test to_json_string() static method '''
         self.assertEqual('[]', Square.to_json_string(None))
 
-    def test__toJsonStringTypeError(self):
+    def test__squareToJsonStringTypeError(self):
         ''' Test to_json_string() static method '''
         self.assertRaises(TypeError, Square.to_json_string)
 
-    def test__fromJsonString(self):
+    def test__squareFromJsonString(self):
         ''' Test from_json_string() static method '''
         s1 = Square(10, 7, 2, 48)
         list_dictionaries = s1.to_dictionary()
         str1 = Square.to_json_string(list_dictionaries)
         self.assertEqual(list_dictionaries, Square.from_json_string(str1))
 
-    def test__fromJsonEmptyString(self):
+    def test__squareFromJsonEmptyString(self):
         ''' Test from_json_string() static method '''
         self.assertEqual([], Square.from_json_string(''))
 
-    def test__fromJsonNoneString(self):
+    def test__squareFromJsonNoneString(self):
         ''' Test from_json_string() static method '''
         self.assertEqual([], Square.from_json_string(None))
 
-    def test__fromJsonStringTypeError(self):
+    def test__squareFromJsonStringTypeError(self):
         ''' Test from_json_string() static method '''
         self.assertRaises(TypeError, Square.from_json_string)
