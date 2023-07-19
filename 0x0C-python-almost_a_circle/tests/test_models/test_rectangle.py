a770c5386cb27cc5f5b87a1cfaccbe92ac6503c6
ahmed <rifkiahmed.91@gmail.com>
2023-07-19 02:41:24 +0000

    Add tests cases for Rectangle class

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -4,115 +4,89 @@ import unittest
 import os
 from json import dumps, loads
 from models.rectangle import Rectangle
-from models.base import Base
 
 
 class TestRectangle(unittest.TestCase):
-    ''' class of tests cases for testing Rectange class '''
+    ''' class of tests cases for testing Rectangle class '''
 
     def setUp(self):
-        ''' Defines variables and initialize instances '''
-        self.args = [10, 20, 30, 40, 50, 60]
+        ''' Defines variables and removes file Rectangle.json '''
+        self.args = [10, 20, 30, 40, 50]
         self.kwargs = {'width': 1, 'height': 3, 'x': 5, 'y': 7, 'id': 99}
-        self.r1 = Rectangle(*self.args[: 5])
-        self.r2 = Rectangle(**self.kwargs)
 
         try:
             os.remove("Rectangle.json")
         except Exception:
             pass
 
+    def tearDown(self):
+        ''' Removes variables and file Rectangle.json '''
+        del self.args, self.kwargs
         try:
-            os.remove("Square.json")
+            os.remove("Rectangle.json")
         except Exception:
             pass
 
-    def tearDown(self):
-        ''' Removes variable '''
-        del self.args, self.kwargs
-
-    def test__init(self):
-        ''' Test __init__() instance method '''
+    def test__Rectangle(self):
+        ''' Test Rectangle class initialisation '''
         self.assertEqual(Rectangle(1, 2).width, 1)
         self.assertEqual(Rectangle(1, 2).height, 2)
         self.assertEqual(Rectangle(1, 2).x, 0)
         self.assertEqual(Rectangle(1, 2).y, 0)
 
-    def test__typeError(self):
-        ''' Test instance type errors '''
-        self.assertRaises(TypeError, Rectangle, *self.args[0: 0])
-        self.assertRaises(TypeError, Rectangle, *self.args[0: 1])
-        self.assertRaises(TypeError, Rectangle, *self.args)
+    def test__rectangleTypeError(self):
+        ''' Test Rectangle class initialisation type error '''
+        self.assertRaises(TypeError, Rectangle)
 
-    def test__width_ValueError(self):
-        ''' Test width value errors '''
-        self.assertRaises(ValueError, Rectangle, -10, 20, 30, 40, 50)
-        self.assertRaises(ValueError, Rectangle, 0, 20, 30, 40, 50)
+    def test__rectangleWidthValueError(self):
+        ''' Test Rectangle instance width value errors '''
+        self.assertRaises(ValueError, Rectangle, -10, 20)
+        self.assertRaises(ValueError, Rectangle, 0, 20)
 
-    def test__width_TypeError(self):
-        ''' Test width type errors '''
-        self.assertRaises(TypeError, Rectangle, 'w', 20, 30, 40, 50)
-        self.assertRaises(TypeError, Rectangle, [1, 1, 1, 1], 20, 30, 40, 50)
-        self.assertRaises(TypeError, Rectangle, None, 20, 30, 40, 50)
+    def test__rectangleWidthTypeError(self):
+        ''' Test Rectangle instance width type error '''
+        self.assertRaises(TypeError, Rectangle, 'w', 20)
 
-    def test__height_ValueError(self):
-        ''' Test height value errors '''
-        self.assertRaises(ValueError, Rectangle, 10, -20, 30, 40, 50)
-        self.assertRaises(ValueError, Rectangle, 10, 0, 30, 40, 50)
+    def test__rectangleHeightValueError(self):
+        ''' Test Rectangle instance width value errors '''
+        self.assertRaises(ValueError, Rectangle, 10, -20)
+        self.assertRaises(ValueError, Rectangle, 10, 0)
 
-    def test__height_TypeError(self):
-        ''' Test  height type errors '''
-        self.assertRaises(TypeError, Rectangle, 10, 'h', 30, 40, 50)
-        self.assertRaises(TypeError, Rectangle, 10, [1, 1, 1, 1], 30, 40, 50)
-        self.assertRaises(TypeError, Rectangle, 10, None, 30, 40, 50)
+    def test__rectangleHeightTypeError(self):
+        ''' Test Rectangle instance width type error '''
+        self.assertRaises(TypeError, Rectangle, 10, 'h')
 
-    def test__x_ValueError(self):
-        ''' Test x value errors '''
-        self.assertRaises(ValueError, Rectangle, 10, 20, -30, 40, 50)
+    def test__rectangle_X_ValueError(self):
+        ''' Test Rectangle instance X value error '''
+        self.assertRaises(ValueError, Rectangle, 10, 20, -30)
 
-    def test__x_TypeError(self):
-        ''' Test x type errors '''
-        self.assertRaises(TypeError, Rectangle, 10, 20, 'x', 40, 50)
-        self.assertRaises(TypeError, Rectangle, 10, 20, [1, 1, 1, 1], 40, 50)
-        self.assertRaises(TypeError, Rectangle, 10, 20, None, 40, 50)
+    def test__rectangle_X_TypeError(self):
+        ''' Test Rectangle instance X type error '''
+        self.assertRaises(TypeError, Rectangle, 10, 20, 'x')
 
-    def test__y_ValueError(self):
-        ''' Test y value errors '''
-        self.assertRaises(ValueError, Rectangle, 10, 20, 30, -40, 50)
+    def test__rectangle_Y_ValueError(self):
+        ''' Test Rectangle instance Y value error '''
+        self.assertRaises(ValueError, Rectangle, 10, 20, 30, -40)
 
-    def test__y_TypeError(self):
-        ''' Test y type errors '''
-        self.assertRaises(TypeError, Rectangle, 10, 20, 30, 'y', 50)
-        self.assertRaises(TypeError, Rectangle, 10, 20, 30, [1, 1, 1, 1], 50)
-        self.assertRaises(TypeError, Rectangle, 10, 20, 30, None, 50)
+    def test__rectangle_Y_TypeError(self):
+        ''' Test Rectangle instance Y type error '''
+        self.assertRaises(TypeError, Rectangle, 10, 20, 30, 'y')
 
-    def test__area(self):
-        ''' Test area() instance method '''
-        self.assertEqual(self.r1.area(), 200)
-        self.assertIsInstance(self.r1.area(), int)
+    def test__rectangleArea(self):
+        ''' Test Rectangle instance area() method '''
+        self.assertEqual(Rectangle(*self.args).area(), 200)
 
-    def test__update(self):
-        ''' Test update() instance method '''
-        r6 = Rectangle(*self.args[: 5])
-        r7 = Rectangle(**self.kwargs)
-        self.r1.update(**self.kwargs)
-        self.assertNotEqual(r6.width, self.r1.width)
-        self.r2.update(*self.args[: 5])
-        self.assertFalse(r7.id == self.r2.id)
-        self.assertRaises(TypeError, Rectangle.update)
-        self.assertRaises(AttributeError, Rectangle.update, None, None)
-        Rectangle.update("welcome")
-        self.assertRaises(AttributeError, Rectangle.update, *self.args)
+    def test__rectangleUpdate(self):
+        ''' Test Rectangle instance update() method '''
+        s1 = Rectangle(*self.args[: 5])
+        s2 = Rectangle(**self.kwargs)
+        s1.update(**self.kwargs)
+        self.assertEqual(s1.width, s2.width)
 
-    def test__to_Dictionary(self):
-        ''' Test to_dictionary() instance method '''
-        self.r1.update(**self.kwargs)
-        d1 = self.r1.to_dictionary()
+    def test__rectangle_to_Dictionary(self):
+        ''' Test Rectangle instance to_dictionary() method '''
+        d1 = Rectangle(**self.kwargs).to_dictionary()
         self.assertEqual(d1, self.kwargs)
-        self.assertIsInstance(d1, dict)
-        self.assertRaises(TypeError, self.r1.to_dictionary, self.r2)
-        self.assertRaises(AttributeError, Rectangle.to_dictionary, None)
-        self.assertRaises(AttributeError, Rectangle.to_dictionary, "Hello")
 
     def test__rectangleDisplay(self):
         ''' Test Rectangle instance display() method '''
@@ -135,92 +109,85 @@ class TestRectangle(unittest.TestCase):
         with open("Rectangle.txt", encoding="utf-8", mode="r") as out_Text:
             self.assertEqual(str1, out_Text.read())
 
-    def test__str(self):
-        ''' Test __str__() instance method '''
-        s1 = '[Rectangle] (50) 30/40 - 10/20'
-        self.assertTrue(s1 == str(self.r1))
+    def test__rectangle__str(self):
+        ''' Test Rectangle instance __str__() method '''
+        s1 = Rectangle(*self.args)
+        output = '[Rectangle] (50) 30/40 - 10/20'
+        self.assertTrue(output == str(s1))
 
-    def test__rectangleDisplayTypeError(self):
-        ''' Test Rectangle instance display() method '''
-        pass
-
-    def test__toJsonString(self):
-        ''' Test to_json_string() static method '''
-        r1 = Rectangle(10, 7, 2, 48)
-        list_dictionaries = r1.to_dictionary()
+    def test__rectangleToJsonString(self):
+        ''' Test super to_json_string() static method '''
+        s1 = Rectangle(10, 7, 2, 48)
+        list_dictionaries = s1.to_dictionary()
         self.assertEqual(
                 dumps(list_dictionaries),
                 Rectangle.to_json_string(list_dictionaries))
 
-    def test__toJsonStringEmptyList(self):
-        ''' Test to_json_string() static method '''
+    def test__rectangleToJsonStringEmptyList(self):
+        ''' Test super to_json_string() static method '''
         self.assertEqual('[]', Rectangle.to_json_string([]))
 
-    def test__toJsonStringNoneList(self):
-        ''' Test to_json_string() static method '''
+    def test__rectangleToJsonStringNoneList(self):
+        ''' Test super to_json_string() static method '''
         self.assertEqual('[]', Rectangle.to_json_string(None))
 
-    def test__toJsonStringTypeError(self):
-        ''' Test to_json_string() static method '''
+    def test__rectangleToJsonStringTypeError(self):
+        ''' Test super to_json_string() static method '''
         self.assertRaises(TypeError, Rectangle.to_json_string)
 
-    def test__fromJsonString(self):
-        ''' Test from_json_string() static method '''
-        r1 = Rectangle(10, 7, 2, 48)
-        list_dictionaries = r1.to_dictionary()
+    def test__rectangleFromJsonString(self):
+        ''' Test super from_json_string() static method '''
+        s1 = Rectangle(10, 7, 2, 48)
+        list_dictionaries = s1.to_dictionary()
         str1 = Rectangle.to_json_string(list_dictionaries)
         self.assertEqual(list_dictionaries, Rectangle.from_json_string(str1))
 
-    def test__fromJsonEmptyString(self):
-        ''' Test from_json_string() static method '''
+    def test__rectangleFromJsonEmptyString(self):
+        ''' Test super from_json_string() static method '''
         self.assertEqual([], Rectangle.from_json_string(''))
 
-    def test__fromJsonNoneString(self):
-        ''' Test from_json_string() static method '''
+    def test__rectangleFromJsonNoneString(self):
+        ''' Test super from_json_string() static method '''
         self.assertEqual([], Rectangle.from_json_string(None))
 
-    def test__fromJsonStringTypeError(self):
-        ''' Test from_json_string() static method '''
+    def test__rectangleFromJsonStringTypeError(self):
+        ''' Test super from_json_string() static method '''
         self.assertRaises(TypeError, Rectangle.from_json_string)
 
     def test__rectangleCreate(self):
-        ''' Test create() instance method '''
+        ''' Test super create() class method '''
         self.assertRaises(TypeError, Rectangle.create(**{'id': 89}))
 
     def test__rectangleSaveToFileNoneList(self):
-        ''' Test save_to_file() class method '''
+        ''' Test super save_to_file() class method '''
         Rectangle.save_to_file(None)
         with open("Rectangle.json", encoding="utf-8", mode="r") as RectFile:
             self.assertEqual('[]', RectFile.read())
 
     def test__rectangleSaveToFileEmptyList(self):
-        ''' Test save_to_file() class method '''
+        ''' Test super save_to_file() class method '''
         Rectangle.save_to_file([])
         with open("Rectangle.json", encoding="utf-8", mode="r") as RectFile:
             self.assertEqual('[]', RectFile.read())
 
     def test__rectangleSaveToFile(self):
-        ''' Test save_to_file() class method '''
-        rectangle1 = Rectangle(1, 2)
-        Rectangle.save_to_file([rectangle1])
+        ''' Test super save_to_file() class method '''
+        s1 = Rectangle(1, 2)
+        Rectangle.save_to_file([s1])
         with open("Rectangle.json", encoding="utf-8", mode="r") as RectFile:
-            str1 = Rectangle.to_json_string([rectangle1.to_dictionary()])
+            str1 = Rectangle.to_json_string([s1.to_dictionary()])
             self.assertEqual(str1, RectFile.read())
 
-    def test__rectangleLoadFromFileNotFound(self):
-        ''' Test load_from__file() class method '''
-        rectangle1 = Rectangle(1, 2)
-        Rectangle.save_to_file([rectangle1])
-        self.assertNotEqual(rectangle1, Rectangle.load_from_file())
-
     def test__rectangleLoadFromFileExist(self):
-        ''' Test load_from__file() class method '''
+        ''' Test super load_from__file() class method '''
+        s1 = Rectangle(1, 2)
+        Rectangle.save_to_file([s1])
+        self.assertNotEqual(s1, Rectangle.load_from_file())
+
+    def test__rectangleLoadFromFileNotFound(self):
+        ''' Test super load_from__file() class method '''
         try:
             os.remove("Rectangle.json")
         except Exception:
             pass
         self.assertEqual([], Rectangle.load_from_file())
-
-    def test__rectangleLoadFromEmptyFile(self):
-        ''' Test load_from__file() class method '''
-        pass
