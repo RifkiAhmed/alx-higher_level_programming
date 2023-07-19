654904343649b8bcd19f5e156eaeee43ae97bc03
ahmed <rifkiahmed.91@gmail.com>
2023-07-19 03:03:21 +0000

    Add tests cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,69 +3,62 @@
 import unittest
 from json import dumps, loads
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
 
 
 class BaseTest(unittest.TestCase):
     ''' class for testing Base class '''
 
-    def test__init__NoneId(self):
-        ''' Test __init__() instance method'''
+    def test__Base(self):
+        ''' Test Base class initialisation without id '''
         b1 = Base()
         self.assertEqual(b1.id, 1)
 
-    def test__init__IntId(self):
-        ''' Test __init__() instance method'''
-        b2 = Base(99)
-        self.assertEqual(b2.id, 99)
+    def test__baseIntId(self):
+        ''' Test Base class initialisation with id of type int '''
+        b1 = Base(99)
+        self.assertEqual(b1.id, 99)
 
-    def test__init__TypeError(self):
-        ''' Test __init__() instance method'''
-        self.assertRaises(TypeError, Base, *(1, 2))
-
-    def test__init_ListId(self):
-        ''' Test __init__() instance method'''
+    def test__baseListId(self):
+        ''' Test Base class initialisation with id of type list '''
         kwargs = {'id': [1, 2]}
         self.assertEqual(Base(**kwargs).id, [1, 2])
 
-    def test__init__TypeError(self):
-        ''' Test __init__() instance method'''
+    def test__baseTypeError(self):
+        ''' Test Base class initialisation type error '''
         self.assertRaises(TypeError, Base, *(1, 2))
 
-    def test__toJsonString(self):
+    def test__baseToJsonString(self):
         ''' Test to_json_string() static method '''
         list_dictionaries = [{"id": 7}, {"id": 10}]
-        self.assertEqual(
-                dumps(list_dictionaries),
+        self.assertEqual(dumps(list_dictionaries),
                 Base.to_json_string(list_dictionaries))
 
-    def test__toJsonStringEmptyList(self):
-        ''' Test to_json_string() static method '''
+    def test__baseToJsonStringEmptyList(self):
+        ''' Test to_json_string() static method with empty list '''
         self.assertEqual('[]', Base.to_json_string([]))
 
-    def test__toJsonStringNoneList(self):
-        ''' Test to_json_string() static method '''
+    def test__baseToJsonStringNoneList(self):
+        ''' Test to_json_string() static method with None type '''
         self.assertEqual('[]', Base.to_json_string(None))
 
-    def test__toJsonStringTypeError(self):
-        ''' Test to_json_string() static method '''
+    def test__baseToJsonStringTypeError(self):
+        ''' Test to_json_string() static method without argument '''
         self.assertRaises(TypeError, Base.to_json_string)
 
-    def test__fromJsonString(self):
-        ''' Test from_json_string() static method '''
+    def test__baseFromJsonString(self):
+        ''' Test from_json_string() static method with string '''
         list_dictionaries = [{"id": 7}, {"id": 10}]
         str1 = Base.to_json_string(list_dictionaries)
         self.assertEqual(list_dictionaries, Base.from_json_string(str1))
 
-    def test__fromJsonEmptyString(self):
-        ''' Test from_json_string() static method '''
+    def test__baseFromJsonEmptyString(self):
+        ''' Test from_json_string() static method with empty string '''
         self.assertEqual([], Base.from_json_string(''))
 
-    def test__fromJsonNoneString(self):
-        ''' Test from_json_string() static method '''
+    def test__baseFromJsonNoneString(self):
+        ''' Test from_json_string() static method with None type '''
         self.assertEqual([], Base.from_json_string(None))
 
-    def test__fromJsonStringTypeError(self):
-        ''' Test from_json_string() static method '''
+    def test__baseFromJsonStringTypeError(self):
+        ''' Test from_json_string() static method without argument '''
         self.assertRaises(TypeError, Base.from_json_string)
