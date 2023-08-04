52e4b1396d9df754f15e6b5d7571d2a3415c6fcd
ahmed <rifkiahmed.91@gmail.com>
2023-08-04 15:10:13 +0000

    Add area() instance method

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-
-'''
-class Square: new type
-'''
+"""
+    Square module
+"""
 
 
 class Square:
-    ''' Represents a square with a zise.'''
-
+    """ Define square """
     def __init__(self, size=0):
-        '''Initialise instance/object'''
+        """ Initialise instances """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
+        """ Returns the current square area """
         return self.__size ** 2
