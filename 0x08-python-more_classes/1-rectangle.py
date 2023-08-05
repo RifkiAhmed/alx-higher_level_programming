f0d9fe18c4f554e1cb21b01fc99a91a8249fee9d
ahmed <rifkiahmed.91@gmail.com>
2023-08-05 12:24:25 +0000

    Encapsulate width and height private instance attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-''' Module: Creates class Rectangle
-'''
+"""
+    Rectangle module
+"""
 
 
 class Rectangle:
-    ''' Defines a Rectangle class
-    '''
+    """ Defines Rectangle """
     def __init__(self, width=0, height=0):
-        ''' Initialize instance
-        '''
-        self.height = height
+        """ Initailises Rectangle instance """
         self.width = width
+        self.height = height
 
     @property
     def width(self):
-        ''' Gets width value
-        '''
+        """ Returns width of current rectangle """
         return self.__width
 
     @width.setter
     def width(self, value):
-        ''' Sets width
-        '''
-        if type(value) is not int:
+        """ Sets value of width for current rectangle """
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -30,15 +27,13 @@ class Rectangle:
 
     @property
     def height(self):
-        ''' Gets height value
-        '''
+        """ Returns height of current rectangle """
         return self.__height
 
     @height.setter
     def height(self, value):
-        ''' Sets height
-        '''
-        if type(value) is not int:
+        """ Sets value of height for current rectangle """
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
