21a9bd359c0893da50ecc6b21a2adb42bdaaf09a
ahmed <rifkiahmed.91@gmail.com>
2023-08-06 13:07:44 +0000

    Add area() and __str__() public instance methods

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-''' Module for a class Rectangle that inherits from BaseGeometry class
-'''
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+    Rectangle module inherits from BaseGeometry module
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    ''' Represents a Rectangle class that inherits from BaseGeometry
-    '''
-
+    """ Defines Rectangle class """
     def __init__(self, width, height):
-        ''' Initialises instance of class Rectangle
-        '''
+        """ Initialises Rectangle instance """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        ''' Calculates area of Rectangle instance
-        '''
+        """ Returns the area of current rectangle """
         return self.__width * self.__height
 
     def __str__(self):
-        ''' Returns a string widht/height representation of Rectange instance
-        '''
-        return '[Rectangle] ' + str(self.__width) + (
-                '/' + str(self.__height))
+        """ Returns a string representation of current rectangle """
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
