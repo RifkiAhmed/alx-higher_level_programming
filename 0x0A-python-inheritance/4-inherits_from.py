b81053bfb31eb1937287b7d07ba31b4bbd33ca14
ahmed <rifkiahmed.91@gmail.com>
2023-08-06 12:22:59 +0000

    Checks if an object type is a subclass of a specified class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-''' Module defines a function thats checks if an object is
-    an instance of of a class that inherits directly or not
-    from a specified class
-'''
+"""
+    inherits_from module
+"""
 
 
 def inherits_from(obj, a_class):
-    ''' Checks if obj is an instance of a_class
-        that inherits directly or not from the
-        specified class
-    '''
-    return issubclass(obj.__class__, a_class) and type(obj) is not a_class
+    """
+        Checks if if the object is an instance of a class that
+        inherited (directly or indirectly) from the specified class
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
