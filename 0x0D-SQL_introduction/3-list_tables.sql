bcd3a00b10da25d3d48f79ea3a05a87c9ef6f0a1
ahmed <rifkiahmed.91@gmail.com>
2023-08-15 13:29:42 -0700

    Show all tables in a database passed as argument

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,4 @@
--- Lists all the tables of a database in your MySQL server.
+-- Lists all the tables of a database in my MySQL server.
 -- This SQL query show all tables in a database passed as argument
 -- of mysql command.
 SHOW TABLES;
