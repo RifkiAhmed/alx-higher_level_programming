67a19329ab23d53f99e6f7942fae953af9801c6c
ahmed <rifkiahmed.91@gmail.com>
2023-08-15 14:44:26 -0700

    Prints the average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Computes the score average of all records in the table second_table.
+-- The database name is passed as an argument of the mysql command.
+-- This SQL query prints average of scores in the table as 'average column'
+SELECT avg(score) as average FROM second_table;
