648e9d71e2b9c19d4b21633c89878396116d96e1
ahmed <rifkiahmed.91@gmail.com>
2023-08-15 15:11:13 -0700

    Fix check

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -2,4 +2,5 @@
 -- The database name is passed as an argument to the mysql command.
 -- This SQL query the score and the number of records for this score
 -- with the label 'number'.
-SELECT score, count(score) as number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS 'number' FROM second_table GROUP BY score
+ORDER BY 'number' DESC;
