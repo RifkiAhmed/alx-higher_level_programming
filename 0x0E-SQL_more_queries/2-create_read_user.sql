8d76f91db568cf93b18a96e1640cd9911df6e81e
Ahmed <rifkiahmed.91@gmail.com>
2023-08-16 13:34:09 +0000

    Create bd and user with SELECT privilege

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@
 -- If the database hbtn_0d_2 or user_0d_2 already exists, script not fail.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'hostname';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
