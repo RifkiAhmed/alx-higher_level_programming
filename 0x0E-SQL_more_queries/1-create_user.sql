0f80f7fd6522ce5c674d499db480e0fc4c089cf0
Ahmed <rifkiahmed.91@gmail.com>
2023-08-16 13:01:02 +0000

    Create user if not exists

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,5 +3,4 @@
 -- If the user user_0d_1 already exists, script not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
-WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
