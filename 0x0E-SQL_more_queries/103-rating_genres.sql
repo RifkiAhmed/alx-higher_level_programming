da07729fdf220c51c39d385da39b6b2eaeab5998
Ahmed <rifkiahmed.91@gmail.com>
2023-08-16 20:29:10 +0000

    Add name of table before name field

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,7 +3,8 @@
 -- Each record display: tv_genres.name - rating sum, sorted in descending
 -- order by their rating, using only one SELECT statement.
 -- The database name is passed as an argument of the mysql command.
-SELECT name, SUM(tv_show_ratings.rate) AS rating FROM tv_genres
+SELECT tv_shows.name AS name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
