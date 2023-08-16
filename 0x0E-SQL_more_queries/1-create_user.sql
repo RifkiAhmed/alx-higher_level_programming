dcab3d53e1941431527ffc393563b278bf827bd7
Ahmed <rifkiahmed.91@gmail.com>
2023-08-16 12:56:44 +0000

    Create user if not exists

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,7 @@
 -- Script that creates the MySQL server user user_0d_1.
 -- user_0d_1 have all privileges, password set to user_0d_1_pwd
 -- If the user user_0d_1 already exists, script not fail
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+CREATE user IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED WITH 'user_0d_1_pwd';
+GRANT all privileges ON *.* TO 'user_0d_1'@'localhost'
+WITH GRANT OPTION
