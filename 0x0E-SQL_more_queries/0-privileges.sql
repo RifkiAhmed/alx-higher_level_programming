1a388eca8574b71bcbfba91ce46c8d108447c1a9
Ahmed <rifkiahmed.91@gmail.com>
2023-08-16 12:24:30 +0000

    Remove single quote

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
 -- These SQL queries  in localhost.
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
