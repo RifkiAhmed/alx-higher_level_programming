7709223e38002968881ea3ff6a9d8895ec12dc26
Ahmed <rifkiahmed.91@gmail.com>
2023-08-16 13:28:55 +0000

    Create bd and user with SELECT privilege

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Script that creates the database hbtn_0d_2 and the user user_0d_2,
+-- user_0d_2 have only SELECT privilege in the database hbtn_0d_2,
+-- The user_0d_2 password set to user_0d_2_pwd.
+-- If the database hbtn_0d_2 or user_0d_2 already exists, script not fail.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'hostname';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'hostname';
