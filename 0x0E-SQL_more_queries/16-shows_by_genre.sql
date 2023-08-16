84ed3dd27a90db930d66cabd6341d9586a170d05
Ahmed <rifkiahmed.91@gmail.com>
2023-08-16 19:36:17 +0000

    Change column name by genre

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,7 +4,7 @@
 -- Each record display: tv_shows.title - tv_genres.name, sorted in ascending
 -- order by the show title and genre name, using only one SELECT statement.
 -- The database name is passed as an argument of the mysql command.
-SELECT tv_shows.title AS title, IFNULL(tv_genres.name, NULL) AS genre
+SELECT tv_shows.title AS title, IFNULL(tv_genres.name, NULL) AS name
 FROM tv_shows LEFT JOIN tv_show_genres on tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY title, genre;
+ORDER BY title, name;
