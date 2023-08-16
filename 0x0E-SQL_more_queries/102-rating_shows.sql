ecea65ebba5d5e6cfa19adecf1469314ed1b0f62
Ahmed <rifkiahmed.91@gmail.com>
2023-08-16 20:17:56 +0000

    Change as keyword to uppercase

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,7 +2,7 @@
 -- Each record should display: tv_shows.title - rating sum, sorted in
 -- descending order by the rating, using only one SELECT statement.
 -- The database name is passed as an argument of the mysql command.
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) as rating FROM tv_shows
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
 INNER JOIN tv_show_ratings
 ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
