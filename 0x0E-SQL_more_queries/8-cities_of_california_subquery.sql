f79f58cc59f542762d586252054c965f15875949
Ahmed <rifkiahmed.91@gmail.com>
2023-08-16 17:43:55 +0000

    Use subquery to lists records

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,12 @@
+-- Script that lists all the cities of California that can be found
+-- in the database hbtn_0d_usa.
+-- The states table contains only one record where name = California
+-- (but the id can be different, as per the example).
+-- Results sorted in ascending order by cities.id,
+-- not allowed to use the JOIN keyword.
+-- The database name is passed as an argument of the mysql command.
+SELECT id, name FROM cities WHERE state_id
+IN (
+    SELECT id FROM states WHERE name = 'California'
+    )
+ORDER BY id DESC;
