e1798067b679460286a1d446ca9ec74f2c047274
Ahmed <rifkiahmed.91@gmail.com>
2023-08-16 20:14:53 +0000

    lists all shows by their rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,8 +2,8 @@
 -- Each record should display: tv_shows.title - rating sum, sorted in
 -- descending order by the rating, using only one SELECT statement.
 -- The database name is passed as an argument of the mysql command.
-SELECT title, SUM(tv_show_ratings.rate) as rating FROM tv_shows
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) as rating FROM tv_shows
 INNER JOIN tv_show_ratings
 ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY title
+GROUP BY tv_shows.title
 ORDER BY rating DESC;
