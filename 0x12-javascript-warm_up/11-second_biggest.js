fdaa6bc5464ca3385b0f1def3a481b7350da50ab
Ahmed <rifkiahmed.91@gmail.com>
2023-09-11 21:59:42 +0000

    change file mode to executable

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function findSecondMax (array) {
+  let i;
+  for (i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1])
+
+
+
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log(0);
+} else {
+  console.log(intParse(process.argv.slice(2)));
+}
