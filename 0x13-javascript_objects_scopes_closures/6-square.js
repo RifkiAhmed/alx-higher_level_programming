6c27360b12d500cc2f2bb7ad2876ad938dec26fc
Ahmed <rifkiahmed.91@gmail.com>
2023-09-12 22:22:35 +0000

    Prints a square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const parentSquare = require('./5-square');
+
+class Square extends parentSquare {
+  charPrint (c) {
+    if (c === undefined) { c = 'X'; }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
