17d06eaf288a16d62e610edc47bf0eb9eab651fe
Ahmed <rifkiahmed.91@gmail.com>
2023-09-12 19:40:09 -0700

    Fix checks

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,20 +3,17 @@ const fileSystem = require('fs');
 
 fileSystem.readFile(process.argv[2], (error, buffer1) => {
   if (error) {
-    console.log(error.message);
-    process.exit(1);
+    console.log(error);
   }
 
   fileSystem.readFile(process.argv[3], (error, buffer2) => {
     if (error) {
-      console.log(error.message);
-      process.exit(1);
+      console.log(error);
     }
 
-    fileSystem.writeFile(process.argv[4], buffer1 + buffer2 + '\n', (error) => {
+    fileSystem.writeFile(process.argv[4], buffer1 + buffer2, (error) => {
       if (error) {
-        console.log(error.message);
-        process.exit(1);
+        console.log(error);
       }
     });
   });
