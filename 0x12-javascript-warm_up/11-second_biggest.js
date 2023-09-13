d23da26daf8e3b1f283db774d689b8bd334d8016
Ahmed <rifkiahmed.91@gmail.com>
2023-09-13 06:51:42 -0700

    Find the second biggest number in a list

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,27 @@
 #!/usr/bin/node
 
 function secondMax (array) {
+  const myArray = [];
   let max1, max2;
-  max1 = max2 = parseInt(array[0]);
   array.forEach((item) => {
-    item = parseInt(item);
-    if (max1 < item) {
-      max2 = max1;
-      max1 = item;
-    } else if (max2 < item) {
-      max2 = item;
-    }
+    myArray.push(parseInt(item));
   });
+  if (myArray.length === 2) {
+    max2 = (myArray[0] > myArray[1] ? myArray[1] : myArray[0]);
+  } else {
+    max1 = max2 = myArray[0];
+    myArray.forEach((item) => {
+      if (max1 < item) {
+        max2 = max1;
+        max1 = item;
+      } else if (max2 < item) {
+        max2 = item;
+      }
+    });
+  }
   return max2;
 }
+
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
