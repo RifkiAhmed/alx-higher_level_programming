eaf5640d90b43474b63216ffaa912352407e1b04
Ahmed <rifkiahmed.91@gmail.com>
2023-09-16 05:27:07 -0700

    Use first() function

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State.id).filter(State.name.like(argv[4])).all()
+    state = session.query(State).filter(State.name.like(argv[4])).first()
     if state:
-        print("{}".format(state[0][0]))
+        print("{}".format(state.id))
     else:
         print("Nothing")
     session.close()
