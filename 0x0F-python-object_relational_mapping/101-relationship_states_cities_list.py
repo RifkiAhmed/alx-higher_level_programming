7c453740897fc7ec51b661364f04ca63eaed9e99
Ahmed <rifkiahmed.91@gmail.com>
2023-09-16 11:59:28 -0700

    List all states and their corresponding cities

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+   Module to list all cities related to a state
+   in hbtn_0e_100_usa database.
+"""
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).join(City).order_by(
+            State.id, City.id).all():
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
+    session.close()
