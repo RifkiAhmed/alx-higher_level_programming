c213ae44f78a034610fcaeecadc94f99b8078a51
Ahmed <rifkiahmed.91@gmail.com>
2023-09-16 10:19:54 -0700

    Build a relationship between State and City tables

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Module that defines a City table
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """Definition of a City class"""
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State", back_populates="cities")
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Model that defines a State table
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Definition of a State class"""
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+            "City",
+            back_populates="state",
+            cascade="all, delete")
