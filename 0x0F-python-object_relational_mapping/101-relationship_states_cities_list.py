47da5c6833d3c6d864aef7bfae7340fa26bf3a27
Ahmed <rifkiahmed.91@gmail.com>
2023-09-16 13:37:54 -0700

    Fix checks

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
    Module to list all states and their related cities
-   in hbtn_0e_100_usa database.
+   in hbtn_0e_101_usa database.
 """
 from sys import argv
 from relationship_state import Base, State
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).join(City).order_by(State.id, City.id).all()
+    states = session.query(State).outerjoin(City).order_by(
+            State.id, City.id).all()
     for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
