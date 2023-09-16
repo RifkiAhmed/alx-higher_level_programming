73588483c0d535d2a37514d00adcce89d67fc45b
Ahmed <rifkiahmed.91@gmail.com>
2023-09-16 12:29:08 -0700

    Fix checks

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).join(City).order_by(State.id, City.id).all()
+    states = session.query(State).order_by(State.id).all()
     for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
