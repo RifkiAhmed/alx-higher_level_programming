69c37472ae3c133a529087833cb273008e2d518d
Ahmed <rifkiahmed.91@gmail.com>
2023-09-15 18:32:45 -0700

    Use LIKE BINARY clause

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-    Model to list all states from the database hbtn_0e_0_usa
+    Model to list all states with name starting with 'N'
+    from the database hbtn_0e_0_usa
 """
 import MySQLdb
 from sys import argv
@@ -14,7 +15,7 @@ if __name__ == "__main__":
         db=argv[3]
     )
     cursor = connection.cursor()
-    sql = "SELECT * from states WHERE name LIKE 'N%' ORDER BY id"
+    sql = "SELECT * from states WHERE name LIKE BINARY 'N%' ORDER BY id"
     cursor.execute(sql)
     rows = cursor.fetchall()
     for row in rows:
