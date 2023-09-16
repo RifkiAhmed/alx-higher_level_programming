0981de42b26b9ac027e91a6815a2419f87d152f5
Ahmed <rifkiahmed.91@gmail.com>
2023-09-16 10:20:36 -0700

    Add state with a city

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+   Module to list all cities in hbtn_0e_6_usa database.
+"""
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = State(name="California")
+    session.add(state)
+    session.add(City(name="San Fransisco", state=state))
+    session.commit()
+    session.close()
