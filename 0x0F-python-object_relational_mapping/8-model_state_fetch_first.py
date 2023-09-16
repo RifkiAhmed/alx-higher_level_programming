da5067d13edad35a1e96ecf5b6eefc2bb53a5389
Ahmed <rifkiahmed.91@gmail.com>
2023-09-15 20:33:53 -0700

    List first state using ORM

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+   Model to list first state in hbtn_0e_6_usa database.
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+    session.close()
