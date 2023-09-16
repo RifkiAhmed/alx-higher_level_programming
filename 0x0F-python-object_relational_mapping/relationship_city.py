79d52af1ad7948309da2d5495fe89dcb40234ae2
Ahmed <rifkiahmed.91@gmail.com>
2023-09-16 10:24:34 -0700

    Use foreign_key

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -13,4 +13,4 @@ class City(Base):
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    state = relationship("State", back_populates="cities")
+    state = relationship("State", foreign_key=state_id)
