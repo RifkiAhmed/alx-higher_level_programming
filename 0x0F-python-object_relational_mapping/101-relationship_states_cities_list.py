07a8be76fdd5be9b576b1a3a63e57ce78ec87d68
Ahmed <rifkiahmed.91@gmail.com>
2023-09-16 12:35:53 -0700

    Fix checks

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -4,7 +4,7 @@
    in hbtn_0e_100_usa database.
 """
 from sys import argv
-from relationship_state import Base, State
+from relationship_state import State
 from relationship_city import City
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
@@ -12,10 +12,9 @@ from sqlalchemy import create_engine
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id)
+    states = session.query(State).order_by(State.id).all()
     for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
