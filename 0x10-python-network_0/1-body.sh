46fb6c799e266d8bc8691bd5c15a1103605ee251
ahmed <rifkiahmed.91@gmail.com>
2023-09-28 13:16:35 +0000

    Print http response body if status code is 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-(curl -si "$1" | grep -q "HTTP/1.1 200 OK") && curl -s "$1"
+(curl -si "$1" | grep -q "200 OK") && curl -s "$1"
