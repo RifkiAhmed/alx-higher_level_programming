f8bbf77855a2ac219ce14ddf405e8c04ab652365
Ahmed <rifkiahmed.91@gmail.com>
2023-09-29 11:09:27 -0700

    Code should not be executed when imported

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,11 +2,14 @@
 """ Python script that takes in a URL, sends a request to the URL and displays
     the body of the response (decoded in utf-8)
 """
-import sys
-import urllib.request
 
-try:
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.read().decode('utf-8'))
-except urllib.error.HTTPError as err:
-    print("Error code:", err.status)
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as err:
+        print("Error code:", err.status)
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,9 +3,12 @@
     a POST request to the passed URL with the email as a parameter,
     and finally displays the body of the response
 """
-import sys
-import requests
 
-parameter = {'email': sys.argv[2]}
-response = requests.post(sys.argv[1], parameter)
-print(response.text)
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    parameter = {'email': sys.argv[2]}
+    response = requests.post(sys.argv[1], parameter)
+    print(response.text)
