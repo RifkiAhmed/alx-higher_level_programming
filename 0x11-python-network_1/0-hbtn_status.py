9fd989c79566ed0537bf604ab3097e2b3bf29202
Ahmed <rifkiahmed.91@gmail.com>
2023-09-29 12:18:36 -0700

    Get content charset

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         content = response.read()
+        content_type = response.headers.get_content_charset()
         print("Body response:")
         print("\t- type:", type(content))
         print("\t- content:", content)
-        print("\t- utf8 content:", content.decode())
+        print("\t- utf8 content:", content.decode(content_type))
