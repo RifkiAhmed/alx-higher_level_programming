37e416ebc7a13b24ec2d8dd0501510bbc378447e
Ahmed <rifkiahmed.91@gmail.com>
2023-09-29 12:13:11 -0700

    Check if id in response JSON data

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,9 +9,12 @@ if __name__ == "__main__":
 
     headers = {
             'Accept': 'application/vnd.github+json',
-            'Authorization': sys.argv[2],
+            'Authorization': f'Bearer {sys.argv[2]}',
             'X-GitHub-Api-Version': '2022-11-28'}
     url = f"https://api.github.com/users/{sys.argv[1]}"
     response = requests.get(url, headers=headers)
     data = response.json()
-    print(data['id'])
+    if 'id' in data:
+        print(data['id'])
+    else:
+        print(None)
