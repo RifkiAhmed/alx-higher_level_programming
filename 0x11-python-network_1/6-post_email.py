712e8a1c98459d3d2c5804b572b2e3fb7bb95846
Ahmed <rifkiahmed.91@gmail.com>
2023-09-29 09:43:23 -0700

    Add Module documentation

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" Python script that takes in a URL and an email, sends a POST request to the
-    passed URL with the email as a parameter, and displays the body of the
-    response (decoded in utf-8)
+""" Python script that takes in a URL and an email address, sends
+    a POST request to the passed URL with the email as a parameter,
+    and finally displays the body of the response
 """
 import sys
 import requests
-import urllib.parse
 
 parameter = {'email': sys.argv[2]}
 response = requests.post(sys.argv[1], parameter)
