47cd5ae2c9d3ab177b522ee1b3d7303308af2db5
Ahmed <rifkiahmed.91@gmail.com>
2023-09-29 08:49:45 -0700

    Send a URL request and handle the error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""
+""" Python script that takes in a URL, sends a request to the URL and displays
+    the body of the response (decoded in utf-8)
 """
 import sys
 import urllib.request
-import urllib.parse
 
 try:
     with urllib.request.urlopen(sys.argv[1]) as response:
