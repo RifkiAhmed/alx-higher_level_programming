500867d53150ead259e51996b9ce155659659821
Rifki_Ahmed <rifkiahmed.91@gmail.com>
2023-10-24 14:00:01 -0700

    Compute the number of tasks completed by user id using an API URL

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const todos = JSON.parse(body);
+  const dict = {};
+  for (let i = 1; i <= 10; i++) {
+    let count = 0;
+    todos.forEach((todo) => {
+      if (todo.userId === i) {
+        if (todo.completed) {
+          count++;
+        }
+      }
+    });
+    dict[i] = count;
+  }
+  console.log(dict);
+});
