2cde18b72791706df4735645488b7026c4b6fb3d
Rifki_Ahmed <rifkiahmed.91@gmail.com>
2023-10-24 13:22:45 -0700

    Print number of movies where a specified character is present

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const films = JSON.parse(body).results;
+  let count = 0;
+  films.forEach((film) => {
+    if ((film.characters).includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count++;
+    }
+  });
+  console.log(count);
+});
