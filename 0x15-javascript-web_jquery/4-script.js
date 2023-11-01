2da3b1fb674fa7354ea62f699173cf1550df9aad
Ahmed <rifkiahmed.91@gmail.com>
2023-11-01 13:50:56 +0000

    Toggle the class of the <header> element when the user clicks on the tag DIV

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+$('DIV#toggle_header').click(function () {
+  const header = $('header');
+  if (header.hasClass('red')) {
+    header.removeClass('red').addClass('green');
+  } else {
+    header.removeClass('green').addClass('red');
+  }
+});
